ba4ed1938792f3e3104b30fa222971ab545135a5
yappy2000d <61788097+yappy2000d@users.noreply.github.com>
2025-09-11 16:28:33 +0000

    Index Renamed

--- webap_captcha/python/test/segment/test_segment.py
+++ webap_captcha/python/test/segment/test_segment.py
@@ -4,9 +4,10 @@ import numpy as np
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
 import utils
+import segment
 
 image = utils.get_captcha_image()
 # image = np.array(Image.open("test/fail cases/CCL_B_0.bmp"))
@@ -18,9 +19,9 @@ image_bin = utils.binarize_image(image, 132)
 
 Image.fromarray(image_bin).save("result/captcha_bin.bmp")
 
-labels_im, num_labels = utils.label(image_bin, background=255)
+labels_im, num_labels = segment.label(image_bin, background=255)
 
-result = utils.segment_characters(labels_im, num_labels)
+result = segment.segment_characters(labels_im, num_labels)
 
 for i, img in enumerate(result):
     Image.fromarray(img.astype(np.uint8)).save(f"result/char_{i}.bmp")
