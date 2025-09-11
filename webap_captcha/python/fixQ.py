b5f1d05b02fb81c482030ba348e368adb38301f1
yappy2000d <61788097+yappy2000d@users.noreply.github.com>
2025-09-11 22:37:44 +0000

    Refactor code for consistency and readability in fixQ.py

--- webap_captcha/python/fixQ.py
+++ webap_captcha/python/fixQ.py
@@ -4,39 +4,34 @@ import numpy as np
 import os
 from uuid import uuid4
 
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
-
 import utils
 import segment
 import tflite
 
-paths = glob('test/eucdist/failed/*.bmp')
+paths = glob("test/eucdist/failed/*.bmp")
 
 for path in paths:
     name = path.split("/")[-1].split(".")[0]
-    if name.count('O') != 1:
+    if name.count("O") != 1:
         continue
 
     img = Image.open(path)
     captcha_answer = tflite.get_captcha_result(np.array(img))
 
-    if captcha_answer != name.replace('O', 'Q'):
+    if captcha_answer != name.replace("O", "Q"):
         continue
-    
+
     img_bin = utils.binarize_image(np.array(img), 138)
     labels_im, num_labels = segment.label(img_bin, background=255)
     result = segment.segment_characters(labels_im, num_labels)
 
-    if not os.path.exists(f"assets/FixQ"):
-        os.makedirs(f"assets/FixQ")
+    if not os.path.exists("assets/FixQ"):
+        os.makedirs("assets/FixQ")
 
-    Image.fromarray(result[name.index('O')]).save(f"assets/FixQ/{uuid4()}.bmp")
+    Image.fromarray(result[name.index("O")]).save(f"assets/FixQ/{uuid4()}.bmp")
 
-Q = glob(f"assets/Q/*.bmp")
-FixQ = glob(f"assets/FixQ/*.bmp")
+Q = glob("assets/Q/*.bmp")
+FixQ = glob("assets/FixQ/*.bmp")
 
 avgq = np.zeros((22, 22), dtype=np.float64)
 
@@ -45,9 +40,9 @@ for q in Q:
 
     canva = np.zeros((22, 22), dtype=np.uint16)
 
-    #將圖片至於中心
+    # 將圖片至於中心
     h, w = img.shape
-    canva[(22 - h) // 2:(22 - h) // 2 + h, (22 - w) // 2:(22 - w) // 2 + w] = img
+    canva[(22 - h) // 2 : (22 - h) // 2 + h, (22 - w) // 2 : (22 - w) // 2 + w] = img
 
     avgq += canva
 
@@ -59,15 +54,13 @@ for q in FixQ:
 
     canva = np.zeros((22, 22), dtype=np.uint16)
 
-    #將圖片至於中心
+    # 將圖片至於中心
     h, w = img.shape
-    canva[(22 - h) // 2:(22 - h) // 2 + h, (22 - w) // 2:(22 - w) // 2 + w] = img
+    canva[(22 - h) // 2 : (22 - h) // 2 + h, (22 - w) // 2 : (22 - w) // 2 + w] = img
 
     avgfixq += canva
 avgfixq = avgfixq / len(FixQ)
 
-final = (avgq + avgfixq)//2
+final = (avgq + avgfixq) // 2
 
 Image.fromarray(np.where(final >= 128, 255, 0).astype(np.uint8)).save("assets/Q.bmp")
-
-
