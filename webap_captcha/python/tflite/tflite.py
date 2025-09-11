f31c0ff29ec284a5b3dc38f92360adaf6d1686f5
LSweetSour <61788097+yappy2000d@users.noreply.github.com>
2025-09-11 13:09:54 +0800

    Refactor string formatting and improve code readability in tflite.py

--- webap_captcha/python/tflite/tflite.py
+++ webap_captcha/python/tflite/tflite.py
@@ -10,18 +10,19 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 from utils import get_captcha_image
 
-model_url = 'https://github.com/NKUST-ITC/NKUST-AP-Flutter/raw/fd06efbc54d3829fcca2c99c3517e1a2c6c903e0/assets/webap_captcha.tflite'
+model_url = "https://github.com/NKUST-ITC/NKUST-AP-Flutter/raw/fd06efbc54d3829fcca2c99c3517e1a2c6c903e0/assets/webap_captcha.tflite"
 
 # Download model if not exists
-model_path = 'webap_captcha.tflite'
+model_path = "webap_captcha.tflite"
 try:
-    with open(model_path, 'rb') as f:
+    with open(model_path, "rb") as f:
         pass
 except FileNotFoundError:
     print("Downloading model...")
     r = requests.get(model_url)
-    with open(model_path, 'wb') as f:
+    with open(model_path, "wb") as f:
         f.write(r.content)
+
 
 def convert2gray(img: np.ndarray) -> np.ndarray:
     """
@@ -35,56 +36,60 @@ def convert2gray(img: np.ndarray) -> np.ndarray:
     else:
         raise ValueError("Unsupported image format")
 
+
 def normalize(img: np.ndarray, w: int, h: int, mean: float, std: float) -> np.ndarray:
     """
     Normalize image.
     """
     # img = (img - mean) / std  # [flutter_tflite](https://github.com/shaqian/flutter_tflite/blob/ce8075a391e02e0cc0bc6c2db950aa961a17d745/example/lib/main.dart#L145)
-    img = img / 255.0           # [captcha_utils](https://github.com/NKUST-ITC/NKUST-AP-Flutter/blob/eb77232a1af1365da5e4610c145798f092c41933/lib/utils/captcha_utils.dart#L118)
+    img = (
+        img / 255.0
+    )  # [captcha_utils](https://github.com/NKUST-ITC/NKUST-AP-Flutter/blob/eb77232a1af1365da5e4610c145798f092c41933/lib/utils/captcha_utils.dart#L118)
 
     return img
 
+
 labels = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
 ]
 
 # img = get_captcha_image()
-img = np.array(Image.open('test/fail cases/CCL_A_0.bmp'))
+img = np.array(Image.open("test/fail cases/CCL_A_0.bmp"))
 
 img_gray = convert2gray(img.astype(np.float32))
 
@@ -99,22 +104,25 @@ for i in range(digitsCount):
     # Crop image
     w = imageWidth // digitsCount
     h = imageHeight
-    target = img_gray[0:h, i*w:(i+1)*w]
+    target = img_gray[0:h, i * w : (i + 1) * w]
 
     img_normalized = normalize(target, w, h, 127.5, 255.0)
 
-    interpreter = Interpreter(model_path=model_path)    
+    interpreter = Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
 
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    input_shape = input_details[0]['shape']
-    interpreter.set_tensor(input_details[0]['index'], img_normalized[np.newaxis, :, :, np.newaxis].astype(np.float32))
+    input_shape = input_details[0]["shape"]
+    interpreter.set_tensor(
+        input_details[0]["index"],
+        img_normalized[np.newaxis, :, :, np.newaxis].astype(np.float32),
+    )
 
     interpreter.invoke()
 
-    output_data = interpreter.get_tensor(output_details[0]['index'])
+    output_data = interpreter.get_tensor(output_details[0]["index"])
 
     digit = labels[np.argmax(output_data)]
     result += digit
