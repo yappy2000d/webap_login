b010c4fba8f16c9b32c09701c134fb2f311a23a4
LSweetSour <61788097+yappy2000d@users.noreply.github.com>
2025-09-15 03:56:42 +0800

    Remove unused file

--- webap_captcha/dart/webap_captcha/bin/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:webap_captcha/eucdist.dart';
-import 'package:webap_captcha/utils.dart';
-
-void main(List<String> arguments) async {
-  final imgA = await readImage('assets/M.bmp');
-
-  print(await getCharacter(imgA));
-}
